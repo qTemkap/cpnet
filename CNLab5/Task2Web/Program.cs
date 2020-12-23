2d2e19231f59847bf3b80b0dc72688d552bf8b9d
ArtemSabelnikov <artem.s@unitspace.top>
2020-12-23 19:24:16 +0200

    lab5

---
+++ CNLab5/Task2Web/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("http://localhost:8000/");
+            StreamReader sr = new StreamReader(stream);
+            string newLine;
+            while ((newLine = sr.ReadLine()) != null)
+                Console.WriteLine(newLine);
+
+            Console.WriteLine("Запрос выполнен");
+            Console.Read();
+        }
+    }
+}
---
+++ CNLab5/Task4SMTP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4SMTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sendMail("smtp.gmail.com", "smtp837@gmail.com", "kwfvtuypcjabbrui", "artemka.relit@gmail.com", "Тема письма", "Тестовая оправка сообщени для проверки работы SMTP ");
+            Console.WriteLine("Сообщение отправлено");
+            Console.Read();
+        }
+        public static void sendMail(string smtpServer, string from, string password, string mailto, string caption, string message, string attachFile = null)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(from);
+                mail.Subject = caption;
+                mail.To.Add(new MailAddress(mailto));                
+                mail.Body = message;
+                if (!string.IsNullOrEmpty(attachFile))
+                    mail.Attachments.Add(new Attachment(attachFile));
+                SmtpClient client = new SmtpClient();
+                client.Host = smtpServer;               
+                client.Credentials = new NetworkCredential(from.Split('@')[0], password);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Send(mail);
+                mail.Dispose();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Mail.Send: " + e.Message);
+            }
+        }
+    }
+}
